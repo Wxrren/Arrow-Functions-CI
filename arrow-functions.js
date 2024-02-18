9f231682130b7b8cc47426bd326bc8e3dd63d21e
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-02-18 18:15:37 +0000

    ADD: defining different version of functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,43 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers(a, b) {
+    //Code block
+    return a+b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    //Code block
+    return a+b;
+}
+
+sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+//const addTwoNumbers2 = (a, b) => (a + b); Also Valid
+const addTwoNumbers2 = (a, b) => (a + b)
+
 // Implicit Returns
 
+//Single parameter
+const saySomething = message => console.log(message);
+saySomething ('Hello there!...General Kenobi');
+
+// No parameter
+const sayHello = () => console.log('hello');
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
