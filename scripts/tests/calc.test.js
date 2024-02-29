59d47f5963cd70921e379fdb1ef7cf749fceb34d
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2024-02-29 01:10:48 +0000

    ADD: Addition test

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,19 @@
+const { describe } = require("yargs");
+const addition = require("../tests/calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+    });
+    describe("Subtract function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Divide function", () => {
+
+    });
+});
